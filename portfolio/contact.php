927e2ee580b6f7d2a86964c65a20fd4cb7da66f6
natorcutt <orcuttna@msu.edu>
2015-04-15 22:38:17 -0400

    added Message form and Contact info

--- portfolio/contact.php
+++ portfolio/contact.php
@@ -18,6 +18,25 @@
 			<li> <a href="contact.html"> Contact </a> </li> 
 			</ul> 
 		</nav> 
+<div class="container"> 
+
+<form action="mail.php" method="POST">
+<p> Name </p> <input type="text" name="name">
+<p> Email </p> <input type="text" name="email">
+<p> Message </p> <textarea name="message" rows="6" cols="25"></textarea><br />
+<input type="submit" value="Send"><input type="reset" value="Clear">
+</form>
+
+<?php $name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "emailaddress@here.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
+?> 
 
 
 
@@ -26,6 +45,8 @@
 
 
 
+
+</div> 
 <footer></footer>
 
 
